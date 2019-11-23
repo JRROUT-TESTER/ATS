85fe5b2a94b81184b0b84d60474c82c7548cc263
JRROUT-TESTER <57475847+JRROUT-TESTER@users.noreply.github.com>
2019-11-23 16:55:52 +0530

    Update College.java

--- College.java
+++ College.java
@@ -1,6 +1,7 @@
 class College{
 	public static void main(String[] args){
 		System.out.println("College main method begin ");
+		System.out.println("College main method begin ");
 			
 			Student s1=new Student(101,"Jrr","CJ",37000);
 			Student s2=new Student(101,"Jre","CJ",37000);
